54a2de047e8449ff515c344e79193293c443da43
mchinh1512 <mchinh1512@gmail.com>
2022-06-30 09:40:38 +0700

    update

--- test/ChinhChef-test.js
+++ test/ChinhChef-test.js
@@ -1,45 +1,45 @@
 import {ethers} from 'hardhat';
 const { expect } = require("chai");
 
-  describe('BearnChef should work', () => {
+  describe('ChinhChef should work', () => {
       it('constructor parameters should be correct', async () => {
-          expect(await bearnChef.owner()).is.eq(deployer.address);
-          expect(await bearnChef.chinhPerBlock()).is.eq(toWei('0.1'));
-          expect(await bearnChef.startBlock()).is.eq(50);
+          expect(await chinhChef.owner()).is.eq(deployer.address);
+          expect(await chinhChef.chinhPerBlock()).is.eq(toWei('0.1'));
+          expect(await chinhChef.startBlock()).is.eq(50);
       });
 
       it('parameters should be correct', async () => {
-          expect(await bearnChef.totalAllocPoint()).is.eq(0);
-          expect(await bearnChef.poolLength()).is.eq(0);
+          expect(await chinhChef.totalAllocPoint()).is.eq(0);
+          expect(await chinhChef.poolLength()).is.eq(0);
       });
 
       it('add', async () => {
-          await bearnChef.add(1000, lpToken.address, false, 100);
-          expect(await bearnChef.poolLength()).is.eq(1);
-          let poolInfo = await bearnChef.poolInfo(0);
+          await chinhChef.add(1000, lpToken.address, false, 100);
+          expect(await chinhChef.poolLength()).is.eq(1);
+          let poolInfo = await chinhChef.poolInfo(0);
           expect(poolInfo.isStarted).is.false;
           expect(poolInfo.lastRewardBlock).is.eq(100);
       });
 
       it('bob deposit 10 BUSD', async () => {
           await expect(async () => {
-              await bearnChef.connect(bob).deposit(0, toWei('10'));
+              await chinhChef.connect(bob).deposit(0, toWei('10'));
           }).to.changeTokenBalances(lpToken, [bob], [toWei('-10')]);
           await mineBlocks(ethers, 50);
-          expect(await bearnChef.pendingBearn(0, bob.address)).is.eq(0);
+          expect(await chinhChef.pendingChinh(0, bob.address)).is.eq(0);
           await mineBlocks(ethers, 51);
           expect(await getLatestBlockNumber(ethers)).is.eq(109);
           await expect(async () => {
-              await bearnChef.connect(bob).withdraw(0, 0);
+              await chinhChef.connect(bob).withdraw(0, 0);
           }).to.changeTokenBalances(chinhToken, [bob], [toWei('1.0')]);
           await mineBlocks(ethers, 10);
-          expect(await bearnChef.pendingBearn(0, bob.address)).is.eq(toWei('1.0'));
+          expect(await chinhChef.pendingChinh(0, bob.address)).is.eq(toWei('1.0'));
       });
 
       it('bob withdraw 5 BUSD', async () => {
           let _beforeChinh = await chinhToken.balanceOf(bob.address);
           await expect(async () => {
-              await bearnChef.connect(bob).withdraw(0, toWei('5'));
+              await chinhChef.connect(bob).withdraw(0, toWei('5'));
           }).to.changeTokenBalances(lpToken, [bob], [toWei('5')]);
           let _afterChinh = await chinhToken.balanceOf(bob.address);
           expect(_afterChinh.sub(_beforeChinh)).is.eq(toWei('1.1'));
@@ -47,10 +47,10 @@ const { expect } = require("chai");
 
       it('bob emergencyWithdraw', async () => {
           await mineBlocks(ethers, 10);
-          expect(await bearnChef.pendingBearn(0, bob.address)).is.eq(toWei('1.0'));
+          expect(await chinhChef.pendingChinh(0, bob.address)).is.eq(toWei('1.0'));
           let _beforeChinh = await chinhToken.balanceOf(bob.address);
           await expect(async () => {
-              await bearnChef.connect(bob).emergencyWithdraw(0);
+              await chinhChef.connect(bob).emergencyWithdraw(0);
           }).to.changeTokenBalances(lpToken, [bob], [toWei('5')]);
           let _afterChinh = await chinhToken.balanceOf(bob.address);
           expect(_afterChinh.sub(_beforeChinh)).is.eq(toWei('0'));
