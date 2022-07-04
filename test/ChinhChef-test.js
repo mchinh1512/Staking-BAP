66a3ec65a368d6af4617ce88d4e07498bc80306a
mchinh1512 <mchinh1512@gmail.com>
2022-07-04 09:45:20 +0700

    update

--- test/ChinhChef-test.js
+++ test/ChinhChef-test.js
@@ -2,15 +2,16 @@ const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 const { expectRevert, time, balance } = require('@openzeppelin/test-helpers');
 
-const ChinhChef = artifacts.require("ChinhChef");
-const ChinhToken = artifacts.require("ChinhToken");
-const CToken = artifacts.require("mock/MockERC20")
+const ChinhChef = require("ChinhChef");
+const ChinhToken = require("ChinhToken");
+const CToken = require("mock/MockERC20")
 
 describe("ChinhChef", ([alice, minter]) => {
-  before(async () => {
+  beforeEach(async () => {
     this.chinh = await ChinhToken.new({ from: minter });
     this.lp1 = await MockERC20.new('LPToken', 'LP1', '1000000', { from: minter });
     this.chef = await ChinhChef.new(this.chinh.address, {from: minter});
+
     await this.chinh.transfer(this.chef.address, {from: minter});
     await this.lp1.transfer(alice, '1000', {from: minter});
   });
@@ -23,6 +24,7 @@ describe("ChinhChef", ([alice, minter]) => {
     await time.advanceBlockTo('170');
     await this.lp1.approve(this.chef.address, '1000', {from: alice});
     assert.equal((await this.chinh,balanceOf(alice)).toString(), '0');
+
     await this.chef.deposit(1, '20', {from: alice});
     await this.chef.withdraw(1, '20', {from: alice});
     assert.equal((await this.chinh.balanceOf(alice)).toString(), '263');
@@ -34,6 +36,7 @@ describe("ChinhChef", ([alice, minter]) => {
     await this.chef.deposit(1, '20', { from: alice });
     await this.chef.deposit(1, '40', { from: alice });
     assert.equal((await this.lp1.balanceOf(alice)).toString(), '1940');
+    
     await this.chef.withdraw(1, '10', { from: alice });
     assert.equal((await this.lp1.balanceOf(alice)).toString(), '1950');
     assert.equal((await this.cake.balanceOf(alice)).toString(), '999');
